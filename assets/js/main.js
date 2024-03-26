4eb8e2b603175e932746b0d88b7616613a250053
Hoang Nguyen <hoang.nguyen@coupa.com>
2024-03-26 11:41:37 -0700

    moar changes!

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,24 +41,20 @@ function loadProcess() {
 
 function deployProcess() {
     bpmnModeler.saveXML({ format: false }, (err, xml) => {
-        const form = new FormData();
         console.log(xml);
-        var blob = new Blob([xml], { type: 'text/plain' });
-        var file = new File([blob], "pizza.bpmn", {type: "text/plain"});
-
-        form.append("name", "pizza");
-        form.append("bpmn", file);
         const options = {
             method: 'POST',
-            body: form,
+            body: xml,
             headers: {
                 'Authorization': 'Bearer SANDTOKEN_test',
+                'Content-Type': 'application/xml',
                 'X-Coupa-Customer-FQDN': 'pizzadehut.coupadev.com',
                 'X-Coupa-Customer-Id': 'pizza-de-hut-007',
             }
         }
         fetch('http://localhost:8080/api/v1/processes', options)
             .then((response) => {
+                alert('Deployed');
                 console.log(response);
             })
     })
