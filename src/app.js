e4d7b7e5437a05d6854b5c72c9bae132a0576b68
Hoang Nguyen <hoang.nguyen@coupa.com>
2024-03-28 11:36:27 -0700

    propeties bar

---
+++ src/app.js
@@ -0,0 +1,103 @@
+import "bpmn-js/dist/assets/diagram-js.css";
+import "bpmn-js/dist/assets/bpmn-js.css";
+
+import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "@bpmn-io/properties-panel/assets/properties-panel.css";
+
+import "./style.less";
+
+import $ from "jquery";
+import BpmnModeler from "bpmn-js/lib/Modeler";
+import axios from "axios";
+
+import { debounce } from "min-dash";
+
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+} from "bpmn-js-properties-panel";
+
+import diagramXML from "../resources/newDiagram.bpmn";
+
+var container = $("#js-drop-zone");
+
+var canvas = $("#js-canvas");
+
+var bpmnModeler = new BpmnModeler({
+  container: canvas,
+  propertiesPanel: {
+    parent: "#js-properties-panel",
+  },
+  additionalModules: [BpmnPropertiesPanelModule, BpmnPropertiesProviderModule],
+});
+
+function createNewProcess() {
+  openDiagram(diagramXML);
+}
+
+function loadProcess() {
+  showOpenFilePicker()
+    .then((files) => {
+      return files[0].getFile();
+    })
+    .then((file) => {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        openDiagram(e.target.result);
+      };
+      reader.readAsText(file);
+    });
+}
+
+function deployProcess() {
+  bpmnModeler.saveXML({ format: false }, (err, xml) => {
+    console.log(xml);
+    const options = {
+      method: "POST",
+      body: xml,
+      headers: {
+        Authorization: "Bearer SANDTOKEN_test",
+        "Content-Type": "application/xml",
+        "X-Coupa-Customer-FQDN": "pizzadehut.coupadev.com",
+        "X-Coupa-Customer-Id": "pizza-de-hut-007",
+      },
+    };
+    fetch("http://localhost:8080/api/v1/processes", options).then(
+      (response) => {
+        alert("Deployed");
+        console.log(response);
+      }
+    );
+  });
+}
+
+async function openDiagram(xml) {
+  try {
+    await bpmnModeler.importXML(xml);
+
+    container.removeClass("with-error").addClass("with-diagram");
+  } catch (err) {
+    container.removeClass("with-diagram").addClass("with-error");
+
+    container.find(".error pre").text(err.message);
+
+    console.error(err);
+  }
+}
+
+$(function () {
+  $("#js-create-process").click(function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    createNewProcess();
+  });
+
+  $("#js-load-process").click(function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    loadProcess();
+  });
+});
